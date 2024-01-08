0c0222d778fbad7ce202ae9d062734831b5d6f18
jaimebranco <jaimebrancofilho@gmail.com>
2024-01-08 13:19:44 -0300

    aulas 12,13 e 14

--- aula012/aula012.js
+++ aula012/aula012.js
@@ -1,15 +1,6 @@
-//let n1=[10,20,30]
-//let n2=[11,22,33,44,55]
-//let n3=[...n1,...n2]
+const objs1=document.getElementsByTagName("div")
+const objs2=[...document.getElementsByTagName("div")]
 
-//console.log("n1: " + n1)
-//console.log("n2: " + n2)
-//console.log("n3: " + n3)
-//console.log("tipo de n3:" + typeof(n3))
 
-const soma=(v1,v2,v3)=>{
-    return v1+v2+v3
-}
-
-let valores=[1,5,4,10]
-console.log(soma(...valores))
+console.log(objs1)
+console.log(objs2)
---
+++ aula013/aula013.js
@@ -0,0 +1,13 @@
+let num=20
+let clima="sol"
+
+if(num > 70 && clima=="sol"){
+    console.log("Vou a praia")
+}
+else {
+    
+    console.log("Vou ao cinema")
+}
+
+console.log("Fim do programa")
+
